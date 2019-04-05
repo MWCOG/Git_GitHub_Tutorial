09ec790d610897e3e6ae1becfeac946d286577da
Ray Ngo <ngovn@outlook.com>
2019-04-05 16:24:16 -0400

    Fixed the potential use of Iter_HWY.net incorrectly

--- Average_Link_Speeds.bat
+++ Average_Link_Speeds.bat
@@ -4,9 +4,9 @@ REM  Average Link Speeds
 
 :: Write loaded links file from assignment to new file
 :: current iteration speeds will be removed and rewritten with averaged speeds below
-
-if exist %_iter_%_HWY.net   copy   %_iter_%_HWY.net    %_iter_%_Assign_Output.net /y
-if exist %_iter_%_HWY.net   copy   %_iter_%_HWY.net    %_iter_%_HWY.tem1 /y
+:: 03/22/2019 RN  Modified lines 8, 9, 21  to address the potential issue of using iter_HWY.NET incorrectly
+if exist temp1_%_iter_%_HWY.net   copy   temp1_%_iter_%_HWY.net    %_iter_%_Assign_Output.net /y
+if exist temp1_%_iter_%_HWY.net   copy   temp1_%_iter_%_HWY.net    %_iter_%_HWY.tem1 /y
 
 
 if exist voya*.*  del voya*.*
@@ -18,7 +18,7 @@ if exist voya*.prn  copy voya*.prn %_iter_%_Average_Link_Speeds.rpt /y
 :: Now copy over the original Loaded file with revised file containing new/avg speeds
 :: Note: the original file from assignment is maintained as %_iter_%_Assigned_%HWY.net
 
-if exist  %_iter_%_Averaged_HWY.net  copy %_iter_%_Averaged_HWY.net %_iter_%_HWY.net /y
+if exist  %_iter_%_Averaged_HWY.net  copy %_iter_%_Averaged_HWY.net temp2_%_iter_%_HWY.net /y
 
 
 goto end
