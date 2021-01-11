dc8ca708d142cf3e06c2dfb4d3ad9f645437ed1f
Ray Ngo <ngovn@outlook.com>
2021-01-11 14:45:15 -0500

    Correct the sub-nodes in the batch files for future scenarios

--- run_Model_2025_Final.bat
+++ run_Model_2025_Final.bat
@@ -12,9 +12,11 @@ set runbat=run_ModelSteps_2025_Final.bat
 ::     Number of subnodes:  1-3 => 3 subnodes and one main node = 4 nodes in total
 set useIdp=t
 set useMdp=t
-::  AMsubnode & MDsubnode are used in highway_assignment_parallel.bat/s
+::  05/10/2019 AMsubnode, MDsubnode, PMsubnode, NTsubnode are used in highway_assignment_parallel.bat/s
 set AMsubnode=1-4
 set MDsubnode=2-4
+set PMsubnode=2-4
+set NTsubnode=2-4
 ::  subnode used in transit fare and transit assignment
 ::  We no longer use IDP in transit skimming, since it would require 16 cores
 set subnode=1-3
